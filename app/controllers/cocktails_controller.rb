5411ccf38726c54e9e7752ce7ca0a3fcbfc8716b
Lyndsey Reed <ler2144@tc.columbia.edu>
2019-11-15 16:50:50 +0100

    mr cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,30 +2,34 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
 end
 
-#   def show
-#     @cocktail = Cocktail.find(params[:id])
-#     @dose = Dose.new
-#     @review = Review.new
-#   end
+private
 
-#   def new
-#     @cocktail = Cocktail.new
-#   end
-
-#   def create
-#     @cocktail = Cocktail.new(cocktail_params)
-#     if @cocktail.save
-#       redirect_to cocktail_path(@cocktail)
-#     else
-#       render 'new'
-#     end
-#   end
-
-#   private
-
-#   def cocktail_params
-#     params.require(:cocktail).permit(:name)
-#   end
-# end
+def cocktail_params
+  params.require(:cocktail).permit(:name)
+end
