ba0b551326d4a2b28fe9772c7aec507c5102da3a
Lyndsey Reed <ler2144@tc.columbia.edu>
2019-11-18 11:47:39 +0100

    mr cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
+  root to: 'pages#home'
   resources :doses, only: :destroy
   resources :cocktails do
     resources :doses, only: [:new, :create]
