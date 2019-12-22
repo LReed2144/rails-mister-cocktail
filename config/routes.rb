79a9b3bcd088b58cfa13a86319b5a49b711446cb
Lyndsey Reed <ler2144@tc.columbia.edu>
2019-12-22 12:44:20 -0500

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "cocktails#index"
-  root to: 'pages#home'
-  resources :doses, only: :destroy
-  resources :cocktails do
-    resources :doses, only: [:new, :create]
-  end
 end
